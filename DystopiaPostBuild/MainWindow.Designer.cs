f3f82248caec65a28ebd452e3a1d7b5ee68ea46d
GLaDOS <leo.durrant@outlook.com>
2018-06-07 21:34:38 +0100

    Let there be code!

---
+++ DystopiaPostBuild/MainWindow.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DystopiaPostBuild
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.CloseTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(12, 11);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(70, 13);
+            this.StatusLabel.TabIndex = 0;
+            this.StatusLabel.Text = "Please wait...";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Controls.Add(this.StatusLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(505, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // logBox
+            // 
+            this.logBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logBox.Location = new System.Drawing.Point(0, 34);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(505, 177);
+            this.logBox.TabIndex = 2;
+            this.logBox.WordWrap = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.Location = new System.Drawing.Point(423, 5);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Visible = false;
+            this.CloseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CloseTimer
+            // 
+            this.CloseTimer.Interval = 3000;
+            this.CloseTimer.Tick += new System.EventHandler(this.CloseTimer_Tick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 211);
+            this.ControlBox = false;
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Executing Post-build functions...";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Timer CloseTimer;
+    }
+}
+
---
+++ DystopiaPostBuild/MainWindow.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DystopiaPostBuild
+{
+    public partial class MainWindow : Form
+    {
+        public string WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                WorkingDirectory = Environment.GetCommandLineArgs().GetValue(1).ToString();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            BuildNumberResult bn = BuildNumber();
+            if (bn.result)
+            {
+                CloseButton.Visible = true;
+                StatusLabel.Text = "Incrementation finished! New build number: " + bn.NewBuildNumber;
+                CloseTimer.Enabled = true;
+            }
+            else
+            {
+                CloseButton.Visible = true;
+                StatusLabel.Text = "Incrementation failed!";
+                AddToLog(String.Format("Build failed:" + Environment.NewLine + "GAME_VERSION_FILE path: {0}" + Environment.NewLine + "GAME_BUILD_FILE path: {1}", bn.GAME_VERSION_FILE, bn.GAME_BUILD_FILE));
+            }
+        }
+
+        public void AddToLog(string logcontent)
+        {
+            logBox.Text += "\n" + logcontent;
+        }
+
+        public BuildNumberResult BuildNumber()
+        {
+            // Increment the build number.
+            BuildNumberResult res = new BuildNumberResult();
+
+            string GAME_VERSION_FILE = Path.Combine(WorkingDirectory, @"Source\DystopiaGame\Public\DystopiaBuildVersion.h");
+            string GAME_BUILD_FILE = Path.Combine(WorkingDirectory, @"Source\DystopiaGame\DystopiaBuild.txt");
+
+            res.GAME_VERSION_FILE = GAME_VERSION_FILE;
+            res.GAME_BUILD_FILE = GAME_BUILD_FILE;
+
+            string BUILD_NUMBER_TEXT = "#define GAME_BUILD_NUMBER";
+            int LINE_NUMBER_TO_REPLACE = 9;
+
+            bool bFilesExist = File.Exists(GAME_VERSION_FILE) && File.Exists(GAME_BUILD_FILE);
+
+            if (bFilesExist)
+            {
+                FileInfo VFInfo = new FileInfo(GAME_VERSION_FILE);
+                FileInfo BFInfo = new FileInfo(GAME_BUILD_FILE);
+
+                VFInfo.IsReadOnly = false;
+                BFInfo.IsReadOnly = false;
+
+                string GAME_BUILD_CONTENT = File.ReadAllText(GAME_BUILD_FILE);
+                int GAME_BUILD_NUMBER = int.Parse(GAME_BUILD_CONTENT);
+                int INCRNUMBER = GAME_BUILD_NUMBER + 1;
+                string GAME_VERSION_CONTENT = File.ReadAllText(GAME_VERSION_FILE);
+
+                if (GAME_VERSION_CONTENT.Contains(BUILD_NUMBER_TEXT))
+                {
+                    int TotalLinesInGameVersionFile = File.ReadAllLines(GAME_VERSION_FILE).GetLength(0) + 1;
+
+                    using (var sr = new StreamReader(GAME_VERSION_FILE))
+                    {
+                        for (int i = 1; i < TotalLinesInGameVersionFile; i++)
+                        {
+                            if (sr.ReadLine().Contains(BUILD_NUMBER_TEXT))
+                            {
+                                AddToLog("New build number generated: " + INCRNUMBER.ToString());
+                                sr.Close();
+
+                                var GameVersionFileLines = new List<string>(File.ReadAllLines(GAME_VERSION_FILE));
+
+                                GameVersionFileLines.RemoveAt(LINE_NUMBER_TO_REPLACE);
+                                GameVersionFileLines.Insert(LINE_NUMBER_TO_REPLACE, (BUILD_NUMBER_TEXT + " " + INCRNUMBER));
+
+                                File.WriteAllLines(GAME_VERSION_FILE, GameVersionFileLines);
+                                File.WriteAllText(GAME_BUILD_FILE, INCRNUMBER.ToString());
+
+                                res.result = true;
+                                res.NewBuildNumber = INCRNUMBER.ToString();
+                                return res;
+                            }
+                        }
+                    }
+                }
+
+                res.result = false;
+                return res;
+            }
+            else
+            {
+                res.result = false;
+                return res;
+            }
+        }
+
+        public class BuildNumberResult
+        {
+            public bool result
+            { get; set; }
+            public string GAME_VERSION_FILE
+            { get; set; }
+            public string GAME_BUILD_FILE
+            { get; set; }
+            public string NewBuildNumber
+            { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CloseTimer_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
